d4d8a9f98b8cac5bea859709b0ff7b0dfbb2b2f5
Nartlada Chantharojwong <hpy1@cdc.gov>
2022-03-14 19:35:10 +0700

    Save aggregate data (CFDashboard.Rdata) instead of individual data (CFMast.Rdata).

--- Code/CFAll.R
+++ Code/CFAll.R
@@ -55,21 +55,59 @@ source(paste0(code_folder, "/tblSection4.R"))
 source(paste0(code_folder, "/LabPCRResult.R"))
 #source(paste0(code_folder, "/LabSero.R")) #Nhong has added data
 
+# Screening page
+df_scr <- tblSection1 %>%
+  mutate(scrdate = floor_date(S1ScreenDate, "month")) %>%
+  group_by(scrdate) %>% 
+  tally()
+df_scrage0 <- tblSection1 %>%
+  summarize(
+    n = n(),
+    min = min(S1Age_Year, na.rm = TRUE),
+    q1 = quantile(S1Age_Year, 0.25, na.rm = TRUE),
+    median = median(S1Age_Year, na.rm = TRUE),
+    mean = round(mean(S1Age_Year, na.rm = TRUE), 1),
+    q3 = quantile(S1Age_Year, 0.75, na.rm = TRUE),
+    max = max(S1Age_Year, na.rm = TRUE)
+  )
+df_scrage1 <- tblSection1 %>%
+  group_by(Province) %>% 
+  summarize(
+    n = n(),
+    min = min(S1Age_Year, na.rm = TRUE),
+    q1 = quantile(S1Age_Year, 0.25, na.rm = TRUE),
+    median = median(S1Age_Year, na.rm = TRUE),
+    mean = round(mean(S1Age_Year, na.rm = TRUE), 1),
+    q3 = quantile(S1Age_Year, 0.75, na.rm = TRUE),
+    max = max(S1Age_Year, na.rm = TRUE)
+  )
+df_scrage2 <- tblSection1 %>%
+  group_by(S1HospitalID) %>% 
+  summarize(
+    n = n(),
+    min = min(S1Age_Year, na.rm = TRUE),
+    q1 = quantile(S1Age_Year, 0.25, na.rm = TRUE),
+    median = median(S1Age_Year, na.rm = TRUE),
+    mean = round(mean(S1Age_Year, na.rm = TRUE), 1),
+    q3 = quantile(S1Age_Year, 0.75, na.rm = TRUE),
+    max = max(S1Age_Year, na.rm = TRUE)
+  )
+df_scrgender <- tblSection1 %>%
+  group_by(S1Gender) %>% 
+  tally()
+df_screnrol <- tblSection1 %>%
+  group_by(OLDCF, CF_Enrol) %>% 
+  tally()
+
 ## Save all data frames in one data file (CFMast.RData) for later use ----------
 save(
   list = c(
-    "tblSection1",
-    "tblSection2",
-    "tblSection3",
-    "tblSection4",
-    # "tblSection4_1",
-    # "tblSection5",
-    # "tblSection5_1",
-    # "tblSection6",
-    # "tblSection7",
-    # "tblSection8",
-    "LabPCRResult"
-    #"LabSero" #Nhong has added data
+    "df_scr",
+    "df_scrage0",
+    "df_scrage1",
+    "df_scrage2",
+    "df_scrgender",
+    "df_screnrol"
   ),
-  file = paste0(data_folder, "/CFMast.Rdata")
+  file = paste0(data_folder, "/CFDashboard.Rdata")
 )
