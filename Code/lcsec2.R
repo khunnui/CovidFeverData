7d7a2cda63014df15eb90a4b68669cb423e8350c
Pornpak Khunatorn <hqf7@cdc.gov>
2023-10-27 12:14:10 +0700

    joine sec1

--- Code/lcsec2.R
+++ Code/lcsec2.R
@@ -7,7 +7,7 @@ lcsec2 <- lcsec2 %>%
   # Rename all column names to lowercase
   rename_all(tolower) %>%
   rename(last_edit_date = `_lasteditdate`) %>% 
-
+  left_join(tblSection1 %>% select (cfid, s1enrolldate, province), by = 'cfid') %>% 
   # Delete unused columns
   select(-c(starts_with("_"))) %>%
   
